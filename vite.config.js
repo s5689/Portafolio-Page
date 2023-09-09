d861688b21ef0e38d3e3ddbc0f318e213ae558e8
s5689 <lemirl0ug@gmail.com>
2023-09-09 20:45:51 -0300

    try 2

--- vite.config.js
+++ vite.config.js
@@ -6,19 +6,19 @@ export default defineConfig({
   build: {
     rollupOptions: {
       input: {
-        main: resolve(__dirname, 'Portafolio-Page/docs/index.html'),
+        main: resolve(__dirname, 'Portafolio-Page/index.html'),
 
-        snake: resolve(__dirname, 'Portafolio-Page/docs/projects/personal/snake/index.html'),
-        mww: resolve(__dirname, 'Portafolio-Page/docs/projects/personal/mwwmp305/index.html'),
-        ascii: resolve(__dirname, 'Portafolio-Page/docs/projects/personal/ascii_paint/index.html'),
-        portafolio: resolve(__dirname, 'Portafolio-Page/docs/projects/personal/portafolio/index.html'),
+        snake: resolve(__dirname, 'Portafolio-Page/projects/personal/snake/index.html'),
+        mww: resolve(__dirname, 'Portafolio-Page/projects/personal/mwwmp305/index.html'),
+        ascii: resolve(__dirname, 'Portafolio-Page/projects/personal/ascii_paint/index.html'),
+        portafolio: resolve(__dirname, 'Portafolio-Page/projects/personal/portafolio/index.html'),
 
-        bingo: resolve(__dirname, 'Portafolio-Page/docs/projects/laboral/bingo/index.html'),
-        calculadora: resolve(__dirname, 'Portafolio-Page/docs/projects/laboral/calculadora/index.html'),
-        dados: resolve(__dirname, 'Portafolio-Page/docs/projects/laboral/dados/index.html'),
+        bingo: resolve(__dirname, 'Portafolio-Page/projects/laboral/bingo/index.html'),
+        calculadora: resolve(__dirname, 'Portafolio-Page/projects/laboral/calculadora/index.html'),
+        dados: resolve(__dirname, 'Portafolio-Page/projects/laboral/dados/index.html'),
       },
     },
 
-    assetsDir: 'Portafolio-Page/docs/assets/',
+    assetsDir: 'Portafolio-Page/assets/',
   },
 });
